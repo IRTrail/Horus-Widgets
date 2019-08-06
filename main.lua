e1410598bb4ecac72f11bd2a0c7f71a58e928909
Ivan <53546870+IRTrail@users.noreply.github.com>
2019-08-05 21:46:42 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,26 +1,23 @@
--- Power panel widget for the Heli Masses!
+-- Heli flight pack battery widget V0.2
 --[[ To do:
       get display to conform to widget size
       maybe add some more info or graphics or bitmaps to make it look better
       ]]
       
   -- Many thanks to DanielGA on Helifreak, who is immensely helpful!
-	-- Also thanks to all those other open sourcers who I egregiously pilfered code from so I could modify it to fit my needs.
 
 local options = {
   { "Sensor", SOURCE }, -- get a sensor
   { "BattSize", VALUE, 35 } -- set battery size to 3500 default
 }
-local pct = nil
-local trip = false
 -- Do some zone stuff. 
 local function create(zone, options)
-  local myZone = { zone=zone, options=options }
--- can these be added to the myZone table and simplify the return call?
+  local myZone = { zone=zone, options=options}
   local capRem = 0
   local capUsed = Sensor
   local capTotal = BattSize
-  return myZone, capRem, capUsed, capTotal
+  
+  return myZone
 end
 -- update options if necessary
 local function update(myZone, options)
@@ -34,7 +31,7 @@ end
 --Function to calculate remaining percentage.
     --Funky math is to allow BattSize reported in actual mAh
 local function calc(sensor, total)
-capRem = math.floor( 100 -(sensor * 100 / total )) -- remaining capacity normalized to pectentage
+  capRem = math.floor( 100 -( sensor * 100 / total )) -- remaining capacity normalized to pectentage
 return capRem
 end
 
@@ -48,49 +45,66 @@ local function getPercentColor(cpercent)
     return lcd.RGB(0,0xff,0)
   end
 end
-
 -- Play percent remaining voice calls
-		-- plays percent at intervals of 10 above 10% and each 5% at intervals below 10%
+    -- plays percent at intervals of 10 above 10% and each 5% at intervals below 10%
 local function bitchAboutIt()
 	local pct
-	-- set up interval
+-- set up interval
 	if capRem < 10 then
 		pct = capRem % 5
 	else
 		pct = capRem % 10
 	end
-	-- bitch about it.
+-- bitch about it.
 	if pct == 0 and capRem ~= trip and capRem > 0 and capRem < 100 then
 		playNumber(capRem, 13)
-		trip = capRem	-- don't keep bitching about it.
+			trip = capRem	-- don't keep bitching about it.
   end
 end
-
 ---------------------------------------------------------------------------
 --             this section contains zone draw functions                 --
 ---------------------------------------------------------------------------
--- Top bar (Tiny)
+-- Top bar, Size: 70x39 (Tiny)
 local function zoneTiny(zone)
   local capUsed = getValue(zone.options.Sensor)
   local capTotal = zone.options.BattSize * 100
   local capRem  = calc(capUsed, capTotal)
- 
-  lcd.drawText(zone.zone.x+2, zone.zone.y+0, capUsed .. " mAh", TEXT_INVERTED_COLOR + SMLSIZE) --report capacity used
+  
+  lcd.drawText(zone.zone.x+0, zone.zone.y+0, capUsed .. "mAh", TEXT_INVERTED_COLOR + SMLSIZE)
+  --lcd.drawText(zone.zone.x+0, zone.zone.y+12, capTotal .. "mAh", TEXT_INVERTED_COLOR + SMLSIZE)--report capacity 
   lcd.setColor(CUSTOM_COLOR, getPercentColor(capRem))
-  lcd.drawGauge(zone.zone.x+0, zone.zone.y+23, 65, 12, capRem, 100, CUSTOM_COLOR)
-  lcd.drawText(zone.zone.x+15, zone.zone.y+20, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED + SMLSIZE)
+  lcd.drawGauge(zone.zone.x+0, zone.zone.y+28, 68, 12, capRem, 100, CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+20, zone.zone.y+25, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED + SMLSIZE)
 end
 
--- Small, Size: 160x30 (1/8)
+-- Small, Size: 160x32 (1/8)
 local function zoneSmall(zone)
-  lcd.drawText(zone.zone.x+8, zone.zone.y+8, "Please use Large size widget.", CUSTOM_COLOR + BLINK + INVERS)
+  local capUsed = getValue(zone.options.Sensor)
+  local capTotal = zone.options.BattSize * 100
+  local capRem  = calc(capUsed, capTotal)
+  
+  lcd.drawText(zone.zone.x+2, zone.zone.y+0, capUsed .. " mAh / " .. capTotal .. " total", TEXT_INVERTED_COLOR + SMLSIZE) --report capacity 
+  lcd.setColor(CUSTOM_COLOR, getPercentColor(capRem))
+  lcd.drawGauge(zone.zone.x+0, zone.zone.y+20, 160, 12, capRem, 100, CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+60, zone.zone.y+16, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED + SMLSIZE)
 end
 -- Medium, Size: 180x70 (1/4)
 local function zoneMedium(zone)
-  lcd.drawText(zone.zone.x+12, zone.zone.y+12, "Please use Large size widget.", CUSTOM_COLOR + BLINK + INVERS)
+  local capUsed = getValue(zone.options.Sensor)
+  local capTotal = zone.options.BattSize * 100
+  local capRem  = calc(capUsed, capTotal)
+  
+  lcd.setColor(CUSTOM_COLOR, WHITE)
+  lcd.drawText(zone.zone.x+2, zone.zone.y+0, "Remaining:", CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+100, zone.zone.y+0, capRem .. "%", CUSTOM_COLOR) --report percent
+  lcd.drawText(zone.zone.x+2, zone.zone.y+16, "Used: " .. capUsed .. "mAh", CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+2, zone.zone.y+32, "Total: " .. capTotal .. "mAh", CUSTOM_COLOR)
+  lcd.setColor(CUSTOM_COLOR, getPercentColor(capRem))
+  lcd.drawGauge(zone.zone.x+0, zone.zone.y+50, 180, 20, capRem, 100, CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+75, zone.zone.y+50, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED)
 end
 
--- Large, Size: 390x170 (half)
+-- Large, Size: 192x152 (half)
 local function zoneLarge(zone)
   local capUsed = getValue(zone.options.Sensor)
   local capTotal = zone.options.BattSize * 100
@@ -100,25 +114,39 @@ local function zoneLarge(zone)
   lcd.drawText(zone.zone.x+2, zone.zone.y+16, "Remaining:", CUSTOM_COLOR)
   lcd.drawText(zone.zone.x+100, zone.zone.y+0, capRem .. "%", CUSTOM_COLOR + DBLSIZE) --report percent
   lcd.drawText(zone.zone.x+2, zone.zone.y+40, "Capacity Used:" , CUSTOM_COLOR)
-  lcd.drawText(zone.zone.x+2, zone.zone.y+56, capUsed .. " mAh", CUSTOM_COLOR) --report capacity used
-  lcd.drawText(zone.zone.x+2, zone.zone.y+80, "Useable Battery:", CUSTOM_COLOR)
-  lcd.drawText(zone.zone.x+2, zone.zone.y+96, capTotal .. " mAh", CUSTOM_COLOR) -- report usable size set in options
+  lcd.drawText(zone.zone.x+2, zone.zone.y+56, capUsed .. "mAh", CUSTOM_COLOR) --report capacity used
+  lcd.drawText(zone.zone.x+2, zone.zone.y+80, "Usable Battery:", CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+2, zone.zone.y+96, capTotal .. "mAh", CUSTOM_COLOR) -- report usable size set in options
   lcd.setColor(CUSTOM_COLOR, getPercentColor(capRem))
-  lcd.drawGauge(zone.zone.x+2, zone.zone.y+120, 170, 20, capRem, 100, CUSTOM_COLOR)
-  lcd.drawText(zone.zone.x+75, zone.zone.y+120, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED)
+  lcd.drawGauge(zone.zone.x+0, zone.zone.y+130, 190, 20, capRem, 100, CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+75, zone.zone.y+130, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED)
 end
--- Large, Size: 390x170 (full)
+-- XLarge, Size: 390x172 (full)
 local function zoneXLarge(zone)
-  lcd.drawText(zone.zone.x+56, zone.zone.y+56, "Please use Large size widget.", CUSTOM_COLOR + BLINK + INVERS)
+  local capUsed = getValue(zone.options.Sensor)
+  local capTotal = zone.options.BattSize * 100
+  local capRem  = calc(capUsed, capTotal)
+    
+  lcd.drawText(zone.zone.x+2, zone.zone.y+0, "Battery", CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+2, zone.zone.y+16, "Remaining:", CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+100, zone.zone.y+0, capRem .. "%", CUSTOM_COLOR + DBLSIZE) --report percent
+  lcd.drawText(zone.zone.x+2, zone.zone.y+40, "Capacity Used:" , CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+2, zone.zone.y+56, capUsed .. "mAh", CUSTOM_COLOR) --report capacity used
+  lcd.drawText(zone.zone.x+2, zone.zone.y+80, "Usable Battery:", CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+2, zone.zone.y+96, capTotal .. "mAh", CUSTOM_COLOR) -- report usable size set in options
+  lcd.setColor(CUSTOM_COLOR, getPercentColor(capRem))
+  lcd.drawGauge(zone.zone.x+2, zone.zone.y+150, 190, 20, capRem, 100, CUSTOM_COLOR)
+  lcd.drawText(zone.zone.x+75, zone.zone.y+150, capRem.."%", TEXT_INVERTED_COLOR + SHADOWED)
 end
--- Voice calls even when the widget isn't visibile.
+-- hope to impliment playValue here as well as refresh() to call percents when widget isn't visible
 local function background(myZone)
   if getRSSI() > 0 then -- play percent if telemetry is linked up
     bitchAboutIt()
   end
 end
--- Draw stuff while the screen is visible.
+-- Draw the values and stuff while the screen is visible.
 function refresh(myZone)
+  
   if getRSSI() > 0 then -- play percent if telemetry is linked up
     bitchAboutIt()
   end
